3b1cf7f1ef2a6158d813df227d487a5a2acd96e2
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-12-07 21:12:04 +0530

    chat updated

--- chat.py
+++ chat.py
@@ -5,10 +5,13 @@ bot = ChatBot('My_Bot')
 def main():
 	while True:
 		msg = input('You: ')
-		if msg == 'Bye' or msg == 'exit':
+		if msg.strip() != 'Bye':
+			result = bot.get_response(msg)                        
+			reply = str(result)
+			print(reply)
+		if msg.strip() == 'Bye':
+			print('Bye')
 			break
-		reply = bot.get_response(msg)
-		print(str(bot.name) + ':' + str(reply))
 
 if __name__ == '__main__':
 	main()
